9599622e4743014ecbbbb527901433b914bd8b5b
allan_hanauer <allan.s.hanauer@gmail.com>
2023-11-18 02:25:42 -0300

    INITIALIZED JOB

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <footer>
+        <p>
+          Challenge by{" "}
+          <a
+            rel="noopener noreferrer"
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">AllanHanauer</a>.
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/endPage.jsx
@@ -0,0 +1,10 @@
+function endPage() {
+  return (
+    <>
+      Thanks for subscribing! A confirmation email has been sent to
+      ash@loremcompany.com. Please open it and click the button inside to
+      confirm your subscription. Dismiss message
+    </>
+  );
+}
+export default endPage;
